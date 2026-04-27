e85cd73b55241555a037cfb3d0c0b0857af502c4
Cristian <cri_1112@hotmail.com>
2026-04-27 20:31:57 -0300

    public service productos

--- public-service/app/main.py
+++ public-service/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routers.rou_categoria_producto import catrouter
+from app.routers.rou_producto import productoRouter
 
 app = FastAPI()
 
 app.include_router(catrouter)
+app.include_router(productoRouter)
 
 @app.get('/')
 def home():
