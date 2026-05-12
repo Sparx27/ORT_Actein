d16664377cb6d1ad122d69174ac1ef8709419bb0
sparx27 <nicolasgcardani@gmail.com>
2026-05-11 21:08:05 -0300

    resolve-conflict haha

--- public-service/app/main.py
+++ public-service/app/main.py
@@ -2,19 +2,3 @@ from fastapi import FastAPI
 from app.routers.rou_categoria_producto import catrouter
 from app.routers.rou_producto import productoRouter
 from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(catrouter)
-app.include_router(productoRouter)
-
-@app.get('/')
-def home():
-  return {'mensaje': 'Servicio encendido'}
