6de72dc1c7d93bb6a434ee6944fd746ba12d7490
Cristian <cri_1112@hotmail.com>
2026-05-22 00:49:14 -0300

    Cambio de filtrado

--- public-service/app/routers/rou_product.py
+++ public-service/app/routers/rou_product.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from app.config.database import get_db
-from app.schemas.sch_product import SchProductDetail, SchFiltersProduct, SchProductPaginated
-from app.services.svc_product import svc_get_products, svc_get_filters, svc_get_product_by_id
+from app.schemas.sch_product import SchProductDetail, SchProductPaginated
+from app.services.svc_product import svc_get_products, svc_get_product_by_id
 
 product_router = APIRouter()
 
@@ -15,13 +15,6 @@ def get_products(
     brand: str | None = Query(default=None)
 ):
     return svc_get_products(db, page, search, category_id, brand)
-
-@product_router.get('/products/filters', response_model=SchFiltersProduct)
-def get_filters(
-    db: Session = Depends(get_db),
-    search: str | None = Query(default=None)
-):
-    return svc_get_filters(db, search)
 
 @product_router.get('/products/{product_id}', response_model=SchProductDetail)
 def get_product(
--- public-service/app/services/svc_product.py
+++ public-service/app/services/svc_product.py
@@ -18,11 +18,17 @@ def svc_get_products(db: Session, page: int, search: str | None, category_id: in
         products = rep_get_products(db,search, LIMIT, offset, category_id, brand)
         total_products = rep_count_products(db, search, category_id, brand)
         total_pages = math.ceil(total_products/LIMIT) if total_products > 0 else 1
+        categories = rep_get_categories_with_products(db, search)
+        brands = [row.marca for row in rep_get_brands(db, search)]
         return {
             'total_products' : total_products,
             'page' : page,
             'total_pages' : total_pages,
-            'products' : products
+            'products' : products,
+            'filters' : {
+                'categories': categories,
+                'brands' : brands
+            }
         }
     except SQLAlchemyError:
         raise HTTPException(status_code=500, detail='Error al obtener los productos')
@@ -35,11 +41,3 @@ def svc_get_product_by_id(db:Session, product_id: int):
         return product
     except SQLAlchemyError:
         raise HTTPException(status_code=500, detail='Error al obtener el producto')
-       
-def svc_get_filters(db: Session, search: str | None):
-    try:
-        categories = rep_get_categories_with_products(db, search)
-        brands = [row.marca for row in rep_get_brands(db, search)]
-        return {'categories': categories, 'brands': brands}
-    except SQLAlchemyError:
-        raise HTTPException(status_code=500, detail='Error al obtener los filtros')
